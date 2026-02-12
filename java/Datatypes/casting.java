473e13a03daf4c3d46bd3c3469f65a32d180663f
nroy12 <nitinroy39613@gamil.com>
2026-02-12 20:18:51 +0530

    Knowing more

---
+++ java/Datatypes/casting.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+public class casting {
+    public static void main(String[] args) {
+        // Casting : 
+        double price = 10.01;
+        double finalprice = price+18;
+        System.out.println(finalprice);
+
+        int p =100;
+        int fp = p + (int)19.2; // casting needed
+        System.out.println(fp);
+
+
+        // constants  -> final
+        final float PI =3.14F;
+
+        
+    }
+}
