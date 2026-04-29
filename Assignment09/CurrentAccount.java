70b1b4384149cd659629bc8226fcdae8c4f5e65b
nroy12 <nitinroy39613@gamil.com>
2026-04-29 10:22:04 +0530

    Added current Account

--- Assignment09/CurrentAccount.java
+++ Assignment09/CurrentAccount.java
@@ -1,5 +1,30 @@
 package AdvanceProgX.Assignment09;
 
-public class CurrentAccount {
-    
+public class CurrentAccount extends Account {
+    private double overdraftLimit;
+
+    public CurrentAccount(String accNo, String name, double balance, double overdraftLimit) {
+        super(accNo, name, balance);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Withdrawal must be positive!");
+        }
+
+        if (amount > getBalance() + overdraftLimit) {
+            throw new IllegalArgumentException("Overdraft limit exceeded!");
+        }
+
+        setBalance(getBalance() - amount);
+    }
+
+    @Override
+    public void display() {
+        super.display();
+        System.out.println("Overdraft Limit: " + overdraftLimit);
+        System.out.println("----------------------");
+    }
 }
