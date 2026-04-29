d8bf0a3b24ab78658e79410ced66f3015bd1f575
nroy12 <nitinroy39613@gamil.com>
2026-04-29 10:23:03 +0530

    Added driver code

--- Assignment09/Main.java
+++ Assignment09/Main.java
@@ -1,5 +1,21 @@
 package AdvanceProgX.Assignment09;
 
+import java.util.*;
 public class Main {
-    
+     public static void main(String[] args) {
+
+        List<Account> accounts = new ArrayList<>();
+
+        accounts.add(new SavingsAccount("S101", "Alice", 1000, 5));
+        accounts.add(new CurrentAccount("C101", "Bob", 500, 300));
+
+        // Operations
+        accounts.get(0).deposit(200);
+        accounts.get(1).withdraw(600);
+
+        // Polymorphism
+        for (Account acc : accounts) {
+            acc.display();
+        }
+    }
 }
